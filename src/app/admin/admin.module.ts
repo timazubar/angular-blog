24fd05aebe6af5b8953a69cc77a4150d8e949a1e
timazubar <timazubar33@gmail.com>
2020-08-11 18:02:33 +0300

    feat: set up login form with validators

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
 import { LoginPageComponent } from './login-page/login-page.component';
@@ -31,6 +34,10 @@ import { EditPageComponent } from './edit-page/edit-page.component';
         ],
       },
     ]),
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatToolbarModule,
   ],
   exports: [RouterModule],
 })
--- src/app/admin/shared/components/admin-layout/admin-layout.component.ts
+++ src/app/admin/shared/components/admin-layout/admin-layout.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
-  styleUrls: ['./admin-layout.component.scss']
+  styleUrls: ['./admin-layout.component.scss'],
 })
 export class AdminLayoutComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  logout(event: Event) {
+    event.preventDefault();
+    this.router.navigate(['/admin', 'login']);
   }
-
 }
