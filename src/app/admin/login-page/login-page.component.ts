50e3f9c1c916faf1c52191fb9ad5a38fed6c3c1e
timazubar <timazubar33@gmail.com>
2020-08-12 08:50:47 +0300

    feat: handle tokens while logging in

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -12,6 +12,7 @@ import { User } from './../shared/interfaces/interfaces';
 })
 export class LoginPageComponent implements OnInit {
   form: FormGroup;
+  submitted = false;
 
   constructor(private auth: AuthService, private router: Router) {}
 
@@ -29,6 +30,7 @@ export class LoginPageComponent implements OnInit {
     if (this.form.invalid) {
       return;
     }
+    this.submitted = true;
 
     const user: User = {
       email: this.form.value.email,
@@ -38,6 +40,7 @@ export class LoginPageComponent implements OnInit {
     this.auth.login(user).subscribe(() => {
       this.form.reset();
       this.router.navigate(['/admin', 'dashboard']);
+      this.submitted = false;
     });
   }
 }
--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -4,17 +4,24 @@ import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 import { environment } from './../../../../environments/environment';
-import { User } from './../interfaces/interfaces';
+import { User, FireBaseAuthResponse } from './../interfaces/interfaces';
 
 @Injectable()
 export class AuthService {
   constructor(private http: HttpClient) {}
 
   get token(): string {
-    return '';
+    const expDate = new Date(localStorage.getItem('fb-token-exp'));
+    if (new Date() > expDate) {
+      this.logout();
+      return null;
+    }
+
+    return localStorage.getItem('fb-token');
   }
 
   login(user: User): Observable<any> {
+    user.returnSecureToken = true;
     return this.http
       .post(
         `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}
@@ -23,13 +30,22 @@ export class AuthService {
       )
       .pipe(tap(this.setToken));
   }
-  logout() {}
+  logout() {
+    this.setToken(null);
+  }
 
   isAuthenticated(): boolean {
     return !!this.token;
   }
 
-  private setToken(res) {
-    console.log(res);
+  private setToken(res: FireBaseAuthResponse | null) {
+    if (res) {
+      const expDate = new Date(new Date().getTime() + +res.expiresIn * 1000);
+      localStorage.setItem('fb-token', res.idToken);
+      localStorage.setItem('fb-token-exp', expDate.toString());
+      console.log(res);
+    } else {
+      localStorage.clear();
+    }
   }
 }
