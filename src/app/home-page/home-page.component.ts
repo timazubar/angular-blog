1abb9fd555b1b621725ceec5a2953fb3805bc16e
timazubar <timazubar33@gmail.com>
2020-08-15 20:48:15 +0300

    feat: dynamically render posts on home-page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Post } from './../admin/shared/interfaces/interfaces';
+import { PostsService } from './../shared/posts.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
+  posts$: Observable<Post[]>;
 
-  constructor() { }
+  constructor(private postsService: PostsService) {}
 
   ngOnInit(): void {
+    this.posts$ = this.postsService.getAll();
   }
-
 }
